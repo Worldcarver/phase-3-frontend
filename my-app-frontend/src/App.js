30ecb8e3898dc984566793ecf3a127c3d1d88492
Worldcarver <ian.murphy92@gmail.com>
2022-07-11 15:04:36 -0400

    fixed issue

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,41 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import MealDisplay from "./components/MealDisplay.js"
+import { useState } from 'react';
 
 
 
 
 function App() {
   const [meals, setMeals] = useState([])
+  const [dishes, setDishes] = useState([])
 
   useEffect(() =>{
     fetch(`http://localhost:9292/meals`)
       .then(res => res.json())
       .then(data => setMeals(data))
   }, [])
-
+  useEffect(() =>{
+    fetch(`http://localhost:9292/dishes`)
+      .then(res => res.json())
+      .then(data => setDishes(data))
+  }, [])
 
 
   return (
@@ -53,7 +37,6 @@ function App() {
 
 
 
->>>>>>> Stashed changes
 }
 
 export default App;
